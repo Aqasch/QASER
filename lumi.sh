087bff3947c20ed0caa1275ca855b9441f263052
Ioana Moflic <moflicioana@gmail.com>
2025-02-25 10:44:50 +0200

    Sbatch script bind project

--- lumi.sh
+++ lumi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-#SBATCH --job-name="qhronos"
+#SBATCH --job-name="qhronos_5"
 #SBATCH --partition=standard-g
 #SBATCH --account project_462000520
-#SBATCH -o lbmt_cobyla_8qH20_F0_energy_untweaked_seed3.out
+#SBATCH -o lbmt_cobyla_8qH20_F0_energy_untweaked_seed5.out
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
@@ -12,4 +12,10 @@
 #SBATCH --time=48:00:00
 #
 #
-srun singularity exec apptainer/images/qhronos.sif python3 main.py --seed 3 --config lbmt_cobyla_8qH2O_step_250_F0_energy_untweaked --experiment_name "finalize/"
+SCRIPTTORUN="
+cd /qhronos
+python3 main.py --seed 5 --config lbmt_cobyla_8qH2O_step_250_F0_energy_untweaked --experiment_name \"finalize/\"
+"
+
+export EXEC="srun singularity exec -B $(pwd):/qhronos apptainer/images/qhronos.sif"
+$EXEC bash -c "$SCRIPTTORUN"
